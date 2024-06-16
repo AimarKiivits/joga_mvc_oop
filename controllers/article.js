aca20e9629e702c59700d2d4d7b6e920ab5dc122
Aimar Kiivits <Aimarkiivits1@gmail.com>
2024-06-16 14:47:15 +0300

    Displayed an article by slug using the article oop model

--- controllers/article.js
+++ controllers/article.js
@@ -3,12 +3,17 @@ const articleModel = new articleDbModel();
 
 class articleController {
     constructor() {
-        const articles = []
+        this.articles = []
     }
 
     async getAllArticles(req, res) {
         const articles = await articleModel.findAll();
         res.status(201).json({articles : articles});
+    }
+
+    async getArticleBySlug(req, res) {
+        const article = await articleModel.findOne(req.params.slug);
+        res.status(201).json({article : article});
     }
 }
 
